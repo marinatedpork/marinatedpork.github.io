33b11e0ac89de2770b81ebc930195c93ca402d3d
Matthew Meyers <m.j.mey3rs@gmail.com>
2015-05-11 13:51:18 -0500

    I hate safari

--- northwoods-revolution/js/plugins/jquery.tubular.1.0.js
+++ northwoods-revolution/js/plugins/jquery.tubular.1.0.js
@@ -40,7 +40,7 @@ var player, playerTwo;
 
         // build container
         var tubularContainer = '<div id="tubular-container" style="overflow: hidden; position: fixed; z-index: 1; width: 100%; height: 100%"><div id="tubular-player" class="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>';
-        var tubularContainerTwo = '<div id="tubular-container-2" style="overflow: hidden; position: fixed; z-index: 1; width: 100%; height: 100%"><div id="tubular-player-2" class="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>';
+        var tubularContainerTwo = '<div id="tubular-container-2" style="overflow: hidden; position: absolute; z-index: 1; width: 100%; height: 100%"><div id="tubular-player-2" class="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>';
 
         // set up css prereq's, inject tubular container and set up wrapper defaults
         $('html,body').css({'width': '100%', 'height': '100%'});
